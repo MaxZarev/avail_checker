2fc30a5ba8c6ec0615c2604c46acd75030334e91
Max Zarev <maksimelizarev@gmail.com>
2024-04-19 14:15:32 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Установка библиотек 
+```bash
+pip install -r requirements.txt
+```
+
+1. Перед запуском проекта переименуйте файл private_keys_example.txt в private_keys.txt
+2. Поместите приватные ключи в файл private_keys.txt
+3. Запустите файл main.py
+
+Обратите внимание что запросы к https://claim.availproject.org происходят без использования прокси
---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+import time
+
+import requests
+from eth_account.messages import encode_defunct
+from web3 import Web3
+
+if __name__ == '__main__':
+    web3 = Web3(Web3.HTTPProvider('https://eth-mainnet.public.blastapi.io'))
+
+    with open('private_keys.txt') as f:
+        private_keys = f.read().splitlines()
+
+    for private_key in private_keys:
+        account = web3.eth.account.from_key(os.getenv("private_key"))
+        timestamp = int(time.time())
+
+        message_text = f'Greetings from Avail!\n\nSign this message to check your eligibility. This signature will not cost you any fees.\n\nTimestamp: {timestamp}'
+        text_hex = "0x" + message_text.encode('utf-8').hex()  # Преобразуем текст в hex
+        text_encoded = encode_defunct(hexstr=text_hex)  # Кодируем текст
+        signed_message = web3.eth.account.sign_message(text_encoded, private_key=account.key)  # Подписываем сообщение
+        signature = signed_message.signature  # Получаем подпись
+        signature = signature.hex()
+
+        post_url = "https://claim-api.availproject.org/check-rewards"
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        data = {
+            "account": account.address.lower(),
+            "type": "ETHEREUM",
+            "timestamp": timestamp,
+            "signedMessage": signature,
+
+        }
+        response = requests.post(post_url, headers=headers, json=data)
+        print(f'{account.address} {response.json()}')
+
